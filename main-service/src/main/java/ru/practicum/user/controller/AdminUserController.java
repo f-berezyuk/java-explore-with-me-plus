b6f395e0ef1a46e317c0ecda426a4cfc8fd12797
f.berezyuk <f.berezyuk@gmail.com>
2024-11-23 13:34:28 +0300

    16 tests left

--- main-service/src/main/java/ru/practicum/user/controller/AdminUserController.java
+++ main-service/src/main/java/ru/practicum/user/controller/AdminUserController.java
@@ -1,5 +1,7 @@
 package ru.practicum.user.controller;
 
+import java.util.List;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -7,12 +9,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.user.dto.UserDto;
 import ru.practicum.user.dto.UserRequestDto;
 import ru.practicum.user.service.UserService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/admin/users")
